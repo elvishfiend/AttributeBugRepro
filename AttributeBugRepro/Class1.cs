0aaf37cb31c41abe00a1aaa7adc193481ec883de
Zac <zac@kogan.com.au>
2018-03-20 10:08:26 +1100

    First Commit

---
+++ AttributeBugRepro/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace AttributeBugRepro
+{
+    public class Class1
+    {
+        [CsvHelper.Configuration.Attributes.Constant("")]
+        public string fullyQualifiedProperty { get; set; }
+
+        [CsvHelper.Configuration.Attributes.Constant("")]
+        public string fullyQualifiedField;
+    }
+}
